2fb0a0ee176ea20746bd6b8379335208e0abf5be
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-03-25 10:30:26 +0100

    Esqueleto

---
+++ src/main/java/gal/usc/mercadovalores/gui/FachadaGUI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gal.usc.mercadovalores.gui;
+
+/**
+ *
+ * @author acmc
+ */
+public class FachadaGUI {
+    
+}
