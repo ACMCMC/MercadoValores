e18c4ac6666d9ca743c352e8aae9d5e006f392ad
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-04-11 22:36:53 +0200

    Script

--- insercion.sql
+++ insercion.sql
@@ -1,3 +1,4 @@
 INSERT INTO usuario_mercado(id, clave, saldo, direccion, telefono, estado) VALUES ('usr1', 'cla', 0.0, 'Calle Av. 1, 1', '687214567', 'SOLICITANDO_ALTA');
-INSERT INTO usuario_mercado(id, clave, saldo, direccion, telefono, estado) VALUES ('usr1', 'cla', 0.0, 'Calle Av. 1, 1', '687214567', 'DADO_DE_ALTA');
+INSERT INTO usuario_mercado(id, clave, saldo, direccion, telefono, estado) VALUES ('usr2', 'cla', 0.0, 'Calle Av. 1, 1', '687214567', 'DADO_DE_ALTA');
+INSERT INTO usuario_mercado(id, clave, saldo, direccion, telefono, estado) VALUES ('usr3', 'cla', 0.0, 'Calle Av. 1, 1', '687214567', 'SOLICITANDO_BAJA');
 INSERT INTO usuario_empresa(id, cif, nombre_comercial, importe_bloqueado) VALUES ('usr1', 'fwewrew3', 'nombre', 0.0);
