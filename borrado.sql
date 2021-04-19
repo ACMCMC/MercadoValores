fb9aa66abdf8133f91639d5df2210049ebbd93af
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-04-19 23:58:23 +0200

    borrado

--- borrado.sql
+++ borrado.sql
@@ -1,9 +1,8 @@
-DROP TABLE beneficios;
-DROP TABLE tener_participaciones;
+DROP TABLE beneficios CASCADE;
+DROP TABLE tener_participaciones CASCADE;
 DROP TABLE usuario_mercado CASCADE;
 DROP TABLE usuario_empresa CASCADE;
 DROP TABLE usuario_inversor;
 DROP TABLE usuario_regulador;
-DROP TABLE anuncio_venta;
 DROP TRIGGER comprueba_participaciones ON anuncio_venta;
 DROP FUNCTION comprueba_participaciones;
