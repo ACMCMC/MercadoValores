4db613a5f9ec164051dd87b3c6b22778484ef389
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-04-22 11:17:50 +0200

    Base del DAO Participaciones

--- src/main/java/gal/usc/mercadovalores/db/DAOParticipaciones.java
+++ src/main/java/gal/usc/mercadovalores/db/DAOParticipaciones.java
@@ -1,11 +1,21 @@
 package gal.usc.mercadovalores.db;
 
 import java.sql.Connection;
+import java.util.Set;
 
 import gal.usc.mercadovalores.aplicacion.Participacion;
+import gal.usc.mercadovalores.aplicacion.UsuarioDeMercado;
+import gal.usc.mercadovalores.aplicacion.UsuarioEmpresa;
 
 public class DAOParticipaciones extends DAO<Participacion> {
     public DAOParticipaciones(Connection con) {
 		super(con);
 	}
+
+    public Set<Participacion> getParticipacionesUsuarioDeMercado(UsuarioDeMercado u) {
+
+    }
+    public Set<Participacion> getParticipacionesUsuarioEmpresa(UsuarioEmpresa u) {
+
+    }
 }
