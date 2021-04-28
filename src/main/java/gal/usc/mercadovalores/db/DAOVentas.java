e189cda02599988fb656a1a1410c6cda8196f0e7
fyrthuz <fyrthuz@gmail.com>
2021-04-28 02:29:52 +0200

    Prueba Ventas V6

--- src/main/java/gal/usc/mercadovalores/db/DAOVentas.java
+++ src/main/java/gal/usc/mercadovalores/db/DAOVentas.java
@@ -249,7 +249,7 @@ public class DAOVentas extends DAO<Participacion> {
                                     Double precioaux=resultSet.getDouble("precio");
                                     
                                     
-                                    if(numCompradas+aux>numero){
+                                    if(numCompradas+aux>numero){//Numero compardas+Numero Ofrecidas>Numero que quiere
                                         //Guardamos los ids para hacer luego update
                                         ids.add(idUsuarioaux);
                                         participacionesVendidas.add(aux);
@@ -294,6 +294,25 @@ public class DAOVentas extends DAO<Participacion> {
                                             sumaSaldos.add(aux*precioaux-precioaux*aux*resultSet.getDouble("comision_en_fecha"));
                                             preparedStatement2.executeUpdate();
                                         }
+                                    }else{
+                                        preparedStatement2 = getConexion()
+                                            .prepareStatement("delete from anuncio_venta " + 
+                                                              "where id1=? and id2=? and fecha=? ");
+                                            preparedStatement2.setString(1, idUsuarioaux);
+                                            preparedStatement2.setString(2, idEmpresaaux);
+                                            preparedStatement2.setTimestamp(3, fecha);
+
+                                            //Cantdad a restar al usuario que compra
+                                            saldoARestar+=aux*precioaux;
+                                            ret-=aux;//Se compraron un numero hasta que llegue a 0
+                                            numCompradas+=aux;
+                                            //Comision
+                                            Comision+=precioaux*aux*resultSet.getDouble("comision_en_fecha");
+
+                                            //Cantidad a sumar a cada usuario que vende(venta total - comisión)
+                                            sumaSaldos.add(aux*precioaux-precioaux*aux*resultSet.getDouble("comision_en_fecha"));
+                                            preparedStatement2.executeUpdate();
+                                    
                                     }
 				} catch (EnumConstantNotPresentException e) {
 					FachadaAplicacion.muestraExcepcion(e);
